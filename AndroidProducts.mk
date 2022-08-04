3bcb8ed2132943807ac3314bcb8a6082d9ba60e7
zaidkhan0997 <kzaid0997@gmail.com>
2022-08-04 06:39:49 +0530

    sweet: Welcome To Project-Zephyrus
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_sweet.mk
+    $(LOCAL_DIR)/sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_sweet-user \
-    evolution_sweet-userdebug \
-    evolution_sweet-eng
+    sweet-user \
+    sweet-userdebug \
+    sweet-eng
--- sweet.mk
+++ sweet.mk
@@ -11,17 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Evolution-X Stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-
-# Bootanimation
+# Define bootanimation resolution.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
 
-# Evolution-X Stuff
-EVO_BUILD_TYPE := OFFICIAL
+# OTA updater.
+CUSTOM_BUILD_TYPE := Official
 
-PRODUCT_NAME := evolution_sweet
+# Gapps build only.
+WITH_GMS := true
+
+PRODUCT_NAME := sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
